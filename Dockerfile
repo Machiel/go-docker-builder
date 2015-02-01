ea12144f9fa039f887cb3b6bf8ac9fc88f538b90
Machiel Molenaar <machiel@machiel.me>
2015-02-01 23:47:36 +0100

    Use Docker Client to try and build images

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,10 @@ FROM golang:latest
 
 VOLUME /output
 
-ADD . /go/src/go-docker-builder
+ADD /Users/Machiel/Documents/Projects/go/src/go-docker-builder /go/src/go-docker-builder
 
 WORKDIR /go/src/go-docker-builder
+
+RUN go get github.com/fsouza/go-dockerclient
 
 ENTRYPOINT ./build.sh
--- repository.go
+++ repository.go
@@ -2,11 +2,26 @@
 package main
 
 import (
+	"archive/tar"
 	"bytes"
+	"github.com/fsouza/go-dockerclient"
+	"io/ioutil"
 	"log"
 	"os/exec"
 	"strconv"
+	"time"
 )
+
+func getDockerClient() *docker.Client {
+	endpoint := "tcp://192.168.59.103:2376"
+	client, err := docker.NewClient(endpoint)
+
+	if err != nil {
+		log.Fatal("Unable to connect to docker: " + err.Error())
+	}
+
+	return client
+}
 
 type Repository struct {
 	ID       int    `json:"id"`
@@ -15,6 +30,8 @@ type Repository struct {
 
 // StartBuild executes a build on the Commit Payload
 func (r Repository) StartBuild() {
+
+	client := getDockerClient()
 
 	repoID := strconv.Itoa(r.ID)
 
@@ -35,17 +52,35 @@ func (r Repository) StartBuild() {
 
 	_ = targetDir + "/output"
 
-	log.Println("Running: docker build " + targetDir)
-	buildCmd := exec.Command("docker", "build", targetDir)
-	tpt, cerr := buildCmd.CombinedOutput()
+	t := time.Now()
 
-	if cerr != nil {
-		log.Println(cerr.Error())
+	inputBuf, outputBuf := bytes.NewBuffer(nil), bytes.NewBuffer(nil)
+
+	dockerFile, err := ioutil.ReadFile(targetDir + "/Dockerfile")
+
+	if err != nil {
+		log.Println("Unable to read dockerfile: " + err.Error())
 		return
 	}
 
-	log.Println(string(tpt))
+	tr := tar.NewWriter(inputBuf)
+	tr.WriteHeader(&tar.Header{Name: "Dockerfile", Size: int64(len(dockerFile)), ModTime: t, AccessTime: t, ChangeTime: t})
+	tr.Write(dockerFile)
+	tr.Close()
 
-	log.Println("Build successful, output")
+	opts := docker.BuildImageOptions{
+		Name:         "made-by-db",
+		InputStream:  inputBuf,
+		OutputStream: outputBuf,
+	}
+
+	err = client.BuildImage(opts)
+
+	if err != nil {
+		log.Println("Unable to build: " + err.Error())
+		return
+	}
+
+	log.Println(string(outputBuf.Bytes()))
 
 }
